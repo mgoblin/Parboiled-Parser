4e2e249cc1e388ee30637acdea5a5e0705a4ad89
Michael Golovanov <mike.golovanov@gmail.com>
2011-10-08 20:34:30 +0400

    Test
    Signed-off-by: Michael Golovanov <mike.golovanov@gmail.com>

--- app/src/main/scala/ru/mg/scala/app/App.scala
+++ app/src/main/scala/ru/mg/scala/app/App.scala
@@ -5,11 +5,8 @@ package ru.mg.scala.app
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
   def main(args : Array[String]) {
     println( "Hello World!" )
-    println("concat arguments = " + foo(args))
   }
 
 }
