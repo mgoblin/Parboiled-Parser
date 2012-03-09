1060a3554da468aacfbddadd0efad9800f1f4d1e
Michael Golovanov <mike.golovanov@gmail.com>
2012-03-09 14:22:36 +0400

    code cleaning

--- app2/src/main/scala/ru/mg/parboiled/ExpressionParser.scala
+++ app2/src/main/scala/ru/mg/parboiled/ExpressionParser.scala
@@ -2,8 +2,9 @@ package ru.mg.parboiled
 
 import org.parboiled.scala._
 
+
 /**
- * Base class for expressions parser.
+ * Trait for expressions parser.
  * <p>
  *   Responsible for whitespace support and parsing expressions like<br/>
  * Primitive ~ Operation ~ Primitive with priority and parens<br/>
@@ -13,39 +14,50 @@ import org.parboiled.scala._
  * First items has higher priority.
  * </p>
  * <p>
- *   Descend classes need to override Primitive and Operations members.
+ *   Descend classes should override Primitive and Operations members.
  * </p>
- */abstract class ExpressionParser[R] extends Parser {
+ */
+trait ExpressionParser[R] extends Parser {
 
-  def Primitive:Rule0
-  def Operations:List[String]
-  def calc(operation:String, op1:R, op2:R): R
+  /**
+   * Primitive is a element of parsing. For example operand in math expression.
+   * Descendants should define primitive
+   * @return primitive rule
+   */
+  def Primitive: Rule0
 
+  /**
+   * List of available operations on primitives. Descendants should define operations.
+   * Item with zero index has highest priority and etc.
+   * Each item can contain operations group. One operation is group corresponds to one char.
+   * @return operations list
+   */
+  def Operations: List[String]
 
   def value(s: String): R
-
-  def InputLine = rule { Expression ~ EOI }
-
-  def Expression:Rule1[R] = rule { Operations.foldLeft(Factor)(opGroup) }
-
-  def Factor = rule { Number | Parens }
-
-  def Parens = rule { WhiteSpace ~ LParen ~ Expression ~ RParen ~ WhiteSpace }
-
-  def Number: Rule1[R] = rule { WhiteSpace ~ (Primitive ~> value) ~ WhiteSpace  }
-
-  def WhiteSpace: Rule0 = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
+  def calc(operation:Char, op1:R, op2:R): R
 
   def LParen = "("
   def RParen = ")"
 
-  def opGroup(e:Rule1[R], operators: String) =  {
-    expr(e, operators.toList.map(_.toString))
+
+  def InputLine = rule { Expression ~ EOI }
+
+  def Expression: Rule1[R] = rule { Operations.foldLeft(Item)(OperationExpression) }
+
+  protected def OperationExpression(e: Rule1[R], opGroup: String) = {
+    e ~ ( zeroOrMore(
+      opGroup.toCharArray.map(op =>
+        String.valueOf(op) ~ e ~~> ((op1:R, op2) => calc(op, op1, op2))
+      ).reduceLeft (_|_)))
   }
 
-  def expr(e:Rule1[R], groupOps: List[String]) = {
-    e ~ ( zeroOrMore(
-        groupOps.map((op) => op ~ e ~~> ((op1:R, op2) => calc(op, op1, op2))).reduceLeft (_|_)))
-  }
+  protected def Item = rule { PrimitiveExpression | ParensExpression }
+
+  protected def ParensExpression = rule { WhiteSpace ~ LParen ~ Expression ~ RParen ~ WhiteSpace}
+
+  protected def PrimitiveExpression: Rule1[R] = rule { WhiteSpace ~ Primitive ~> value ~ WhiteSpace}
+
+  protected def WhiteSpace: Rule0 = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
 
 }
--- app2/src/main/scala/ru/mg/parboiled/math/MathParser.scala
+++ app2/src/main/scala/ru/mg/parboiled/math/MathParser.scala
@@ -1,21 +1,19 @@
 package ru.mg.parboiled.math
 
 import org.parboiled.scala._
-import ru.mg.parboiled.ExpressionParser
+import ru.mg.parboiled.{ExpressionParser}
 
 class MathParser extends ExpressionParser[Int] {
+
   def Primitive = rule { oneOrMore("0" - "9") }
   def Operations = List("*/", "-+")
 
-  def value(v: String) = {
-    println("Value " + v)
-    v.toInt
-  }
+  def value(v: String) = { v.toInt }
 
-  def calc(operation: String, op1: Int, op2: Int) = operation match {
-    case "+" => op1 + op2
-    case "-" => op1 - op2
-    case "*" => op1 * op2
-    case "/" => op1 / op2
+  def calc(operation: Char, op1: Int, op2: Int) = operation match {
+    case '+' => op1 + op2
+    case '-' => op1 - op2
+    case '*' => op1 * op2
+    case '/' => op1 / op2
   }
 }
